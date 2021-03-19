8ba6280e79a227b964d34af3c5f56600cd6847ee
noda <noda@team-lab.com>
2021-03-19 21:55:58 +0900

    シミュレーションの場合はログを送信しない

--- src/dynamodb.py
+++ src/dynamodb.py
@@ -3,13 +3,16 @@ import boto3
 
 def dynamo_record_create(simulation, create_at, project_name, algorithm, profit, price, buying, selling):
     """
-    DynamoDBへログを送信
+    DynamoDBへログを送信（本番のみ）
     """
+    if simulation:
+        return
+
     try:
         dynamo_db = boto3.resource('dynamodb')
 
         # DynamoDBのテーブル名
-        table_name = 'coincheck-auto-trade-simulation' if simulation else 'coincheck-auto-trade'
+        table_name = 'coincheck-auto-trade'
         table = dynamo_db.Table(table_name)
 
         table.put_item(
